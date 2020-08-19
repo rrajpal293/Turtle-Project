8fa79da61a0f8eb57376958b52b417800f7556ab
rrajpal293 <67572919+rrajpal293@users.noreply.github.com>
2020-08-18 23:51:01 -0400

    Create HistData.py

---
+++ HistData.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+
+
+class HistData():  
+    # Inputs & intermediate values
+    _data = None
+    _numDates = None
+    _description = None
+
+    def __init__(self, strFileName, strDataName):
+        df = pd.read_excel(strFileName, nBreakout = 20)
+        # rename PX_LAST column to Close
+        df = df.rename(columns = {'PX_LAST':'Close'}) 
+        self._numdates = len(df)
+        self._data = df
+        self._description = strDataName
+        
+    def Data(self):
+        return self._data
+
+    def Name(self):
+        return self._description
+
+    def NumDates(self):
+    	return self._numDates
