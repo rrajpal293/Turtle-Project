91ec714958845c54c6598e11dc3268f77b70f885
rrajpal293 <67572919+rrajpal293@users.noreply.github.com>
2020-08-20 10:44:53 -0400

    Update SimResults.py

--- SimResults.py
+++ SimResults.py
@@ -11,27 +11,26 @@ class SimResults():
 
     def __init__(self):
     	# create a blank simulation results dataframe
-        df = pd.DataFrame(columns=['Date', 'Signal', 'Entry Level', 'Exit Level','Position Units'])
+        df = pd.DataFrame(columns=['Date', 'Signal', 'Entry Level', 'Exit Level','BOD Units', 'EOD Units'])
         _data - df
         
     def Data(self):
         return self._data
 
     def Append(self, dt, Signal, EntryLevel, ExitLevel, Units):
-    	if sum(_data['Date'] == dt) == 0: # if date does not exist then append
-    		_data.append({'data' : dt, 'Signal': Signal, 'Entry Level' : EntryLevel, 'Exit Level' : ExitLevel, 'Position Units' : Units})
+    	if sum(_data['Date'] == dt) == 0: # if date does not exist then append otherwise update
+    		_data.append({'data' : dt, 'Signal': Signal, 'Entry Level' : EntryLevel, 'Exit Level' : ExitLevel, 'BOD Units' : BODUnits, 'EODUnits' : EODUnits})
     	else:
     		self.Update(dt, 'Signal',Signal)
     		self.Update(dt, 'Entry Level', EntryLevel)
     		self.Update(dt, 'Exit Level', ExitLevel)
-    		self.Update(dt, 'Position Units', Units)
-
+    		self.Update(dt, 'BOD Units', Units)
+    		self.Update(dt, 'EOD Units', Units)
 
     def Update(self, dt, strField, value):
-        if sum(_data['Date'] == dt) == 1: # only update if date already exists, othwrwise append
+        if sum(_data['Date'] == dt) == 1: # only update if date already exists, otherwise append
         	df.loc[df['Date'] == dt, strField] = value
         else:
         	_data.append({'data' : dt, strField : value})
 
     def Evaluate(self):
-    	return self._numDates
