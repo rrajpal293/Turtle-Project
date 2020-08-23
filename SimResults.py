aca2c46ae4818b59e3ed72557dc9807e2c25f63e
rrajpal293 <67572919+rrajpal293@users.noreply.github.com>
2020-08-23 12:43:58 -0400

    Update SimResults.py

--- SimResults.py
+++ SimResults.py
@@ -11,15 +11,15 @@ class SimResults():
 
     def __init__(self):
     	# create a blank simulation results dataframe
-        df = pd.DataFrame(columns=['Date', 'Signal', 'Entry Level', 'Exit Level','BOD Units', 'EOD Units'])
+        df = pd.DataFrame(columns=['Date', 'Signal', 'Entry Level', 'Exit Level','BOD Units', 'EOD Units', 'DailyReturn'])
         self._data = df
         
     def Data(self):
         return self._data
 
-    def Append(self, dt, Signal, EntryLevel, ExitLevel, BODUnits, EODUnits):
+    def Append(self, dt, Signal, EntryLevel, ExitLevel, BODUnits, EODUnits, Strat_Return):
     	#if sum(self._data['Date'] == dt) == 0: # if date does not exist then append otherise update
-        self._data = self._data.append({'Date' : dt, 'Signal': Signal, 'Entry Level' : EntryLevel, 'Exit Level' : ExitLevel, 'BOD Units' : BODUnits, 'EOD Units' : EODUnits}, ignore_index = True)
+        self._data = self._data.append({'Date' : dt, 'Signal': Signal, 'Entry Level' : EntryLevel, 'Exit Level' : ExitLevel, 'BOD Units' : BODUnits, 'EOD Units' : EODUnits, 'DailyReturn': Strat_Return}, ignore_index = True)
     	#else:
     	#	self.Update(dt, 'Signal',Signal)
     	#	self.Update(dt, 'Entry Level', EntryLevel)
@@ -32,3 +32,7 @@ class SimResults():
         	df.loc[df['Date'] == dt, strField] = value
         else:
         	self._data.append({'data' : dt, strField : value})
+    def DrawPNL(self):
+        self._data[['DailyReturn']].cumsum().plot()
+    def TotalReturn(self):
+        return self._data[['DailyReturn']].sum().apply(np.exp)
